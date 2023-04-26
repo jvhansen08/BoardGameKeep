bd882d68e2b381461b82c275a71786930abc337d
Logan Ballard <ballard.logan702@gmail.com>
2023-04-26 13:54:29 -0600

    add firebase.ts

---
+++ Frontend/src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwAYbrAbnBKUFrtUDUpa45rmFrSw1IpBI",
+  authDomain: "boardgamersparadise.firebaseapp.com",
+  projectId: "boardgamersparadise",
+  storageBucket: "boardgamersparadise.appspot.com",
+  messagingSenderId: "1089265879000",
+  appId: "1:1089265879000:web:3a4cf45df8fbfe0262d4ad",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
