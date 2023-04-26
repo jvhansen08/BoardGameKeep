448f27910ceb83acc9553121b24214f49c80213d
LandonDoyle7599 <landondoyle7599@gmail.com>
2023-04-26 15:58:45 -0600

    homepage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { CreateAccount } from "./pages/CreateAccount";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Login } from "./pages/Login";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Home/>,
     children: [
       {
         path: "/login",
