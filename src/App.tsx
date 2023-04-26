f5ee3c07f8d278ef61f33d68c094212ba2eca812
Aaron Hanks <theaaronhanks@gmail.com>
2023-04-26 16:20:48 -0600

    test api

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,12 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+    useEffect(() => {
+        console.log("Hello World");
+        fetch("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=games&location=41.7759,-111.8068&radius=50000&key=AIzaSyCjjeTkUXYJ_HafVKsfPkmBHwJ3GM1AYDQ")
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }, []);
   return (
     <>
       <RouterProvider router={router} />
